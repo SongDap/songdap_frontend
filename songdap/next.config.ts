965729a0e19fe958c017888a1eaf4666d1a6b133
NaldonStudy <naldonstudy@gmail.com>
2025-12-27 15:30:30 +0900

    feat: CICD를 위한 설정

--- songdap/next.config.ts
+++ songdap/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* 정적 배포를 위한 설정 */
+  output: 'export',
+
+  /* 정적 배포 시 이미지 최적화 오류 방지 */
+  images: {
+    unoptimized: true,
+  },
+
+  /* 기존 설정 유지 */
   reactCompiler: true,
 };
 
