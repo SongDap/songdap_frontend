4888a65dbf8b1302f6b58d369e6707698340c39a
송영재 <thddudwo125@gmail.com>
2026-01-21 20:17:10 +0900

    landingpage renewal

---
+++ songdap/src/app/signup/page.tsx
@@ -0,0 +1,18 @@
+import { LandingMain } from "@/features/landing";
+import Link from "next/link";
+
+export default function Page() {
+  return (
+    <div className="flex justify-center items-start min-h-screen bg-white mt-10">
+      
+      <main className="w-full max-w-[728px] px-5">
+        
+        <div className="border-2 border-dashed border-red-500 bg-gray-50 min-h-[400px]">
+
+          
+        </div>
+
+      </main>
+    </div>
+  );
+}
